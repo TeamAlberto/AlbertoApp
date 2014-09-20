0e02234946295dba61a381bd0d4d790b08e9e46a
Justin Halsall <justin@juice10.com>
2014-09-20 14:47:59 +0200

    get correct response from API

--- app/scripts/views/cart.js
+++ app/scripts/views/cart.js
@@ -25,17 +25,17 @@ define([
 
         initialize: function () {
             this.cartItems = new Backbone.Collection();
-            this.searchItems = new Backbone.Collection();
+            var ItemsCollection = Backbone.Collection.extend({
+                parse: function(response) {
+                    return response.products;
+                }
+            });
+            this.searchItems = new ItemsCollection();
             this.fetchSearchItems = _.debounce(function () {
-                // this.searchItems.add({
-                //     name: 'Heineken 12 pack',
-                //     image: 'http://deerparkpub.com/wp-content/uploads/2014/07/Heineken.jpg',
-                //     price: 3.5,
-                // });
                 this.searchItems.fetch();
             }.bind(this), 300);
-            this.model = new Backbone.Model({collection: this.searchItems});
             window.x = this;
+            this.model = new Backbone.Model({collection: this.searchItems});
             this.listenTo(this.searchItems, 'reset', this.renderItems);
             this.listenTo(this.searchItems, 'add', this.renderItems);
             this.listenTo(this.searchItems, 'remove', this.renderItems);
@@ -48,6 +48,7 @@ define([
         },
 
         renderItems: function () {
+          console.log('rendering items');
             var $items = this.$('.items').empty();
             this.searchItems.each(function (item) {
                 var view = new CartItemView({model: item});
